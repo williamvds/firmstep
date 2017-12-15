21850a262ce8526b8c152d55001451e4ef4ef43d
williamvds <w.vigolodasilva@gmail.com>
2017-12-15 11:46:56 +0000

    js: ajax, templating, creating elements

---
+++ js/app.js
@@ -0,0 +1,63 @@
+'use strict'
+
+var I = document.getElementById;
+
+// send an AJAX request to given url, call done() when complete
+function ajax(url, done){
+  var req = new XMLHttpRequest();
+  req.open('GET', url, true);
+  req.send();
+
+  req.onreadystatechange = function(){
+    if (req.readyState != XMLHttpRequest.DONE || req.status == 500) return;
+
+    done(JSON.parse(req.responseText), req);
+  }
+
+  return req;
+}
+
+// template of item element (excluding surrounding li)
+var itemTemplate = '<a href="#" class="product-photo">'
+  +'<img src="{{img}}" height="130" alt="{{name}}">'
+  +'</a>'
+  +'<h2><a href="#">{{name}}</a></h2>'
+  +'<ul class="product-description">'
+  +'<li><span>Manufacturer: </span>{{manufacturer}}</li>'
+  +'<li><span>Storage: </span>{{storage}} GB</li>'
+  +'<li><span>OS: </span>{{os}}</li>'
+  +'<li><span>Camera: </span>{{camera}} Mpx</li>'
+  +'<li><span>Description: </span>{{description}}</li>'
+  +'</ul>'
+  +'<p class="product-price">{{price}}</p>';
+
+// recursively substitute dict values into string by replacing keys
+function subKeyValues(str, details) {
+  for (var key in details) {
+    var val = details[key];
+    if (typeof val === 'object') {
+      // recurse if this is a sub-dict
+      str = subKeyValues(str, val);
+      continue;
+    }
+
+    // replace pattern {{key}} with respective value
+    str = str.replace(new RegExp('{{'+ key +'}}', 'g'),
+      val);
+  }
+
+  return str;
+}
+
+// create a new item element, fill info, and return it
+function newItemHTML(details) {
+  var li = document.createElement('li');
+
+  // form HTML from template
+  var html = subKeyValues(itemTemplate, details);
+  // set HTML
+  li.innerHTML = html;
+
+  return li;
+}
+
